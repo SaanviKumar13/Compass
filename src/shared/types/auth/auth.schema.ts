9a4628b3e0b3eab2220bacb16de2964b7257a3ac
Utkarshini <utkarshininarayan@gmail.com>
2024-03-15 18:28:44 +0530

    Refactor user signup functionality

--- src/shared/types/auth/auth.schema.ts
+++ src/shared/types/auth/auth.schema.ts
@@ -3,6 +3,16 @@ import { z } from 'zod';
 export const AuthSchema = z.object({
   email: z.string().email({ message: 'Invalid email format' }),
   password: z.string().min(5, { message: 'Must be 5 characters long or more' }),
+  name: z.string(),
+  linkedin: z.string().url({ message: 'Invalid LinkedIn URL' }),
+  github: z.string().url({ message: 'Invalid GitHub URL' }), 
+  phone: z.string(), 
+  dob: z.string(), 
+  year: z.string(), 
+  degree: z.string(), 
+  semester: z.string(), 
+  skills: z.array(z.string()), 
+  interests: z.array(z.string()), 
 });
 
 export const AuthGetSchema = z.object({
